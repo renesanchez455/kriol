fcb21266cfebead8214fc580ccd783541f6b7362
renesanchez455 <renesanchez455@yahoo.com>
2022-09-08 23:29:40 -0600

    Updated directories

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,55 +2,9 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"time"
 )
 
-// The application version number
-const version = "1.0.0"
-
-// The configuration settings
-type config struct {
-	port int
-	env  string // development, staging, production, etc.
-}
-
-// Dependency injection
-type application struct {
-	config config
-	logger *log.Logger
-}
-
 func main() {
-	var cfg config
-	// read in the flags that are needed to populate our config
-	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.env, "env", "development", "Environment (development | staging | production)")
-	flag.Parse()
-	//Create a logger
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-	//Create an instance of our application struct
-	app := &application{
-		config: cfg,
-		logger: logger,
-	}
-	// Create our new servemux
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-	//Create our HTTP server
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
-	// Start our server
-	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
-	logger.Fatal(err)
+	fmt.Println("A journey of a thousand miles begins with a single step.")
 }
