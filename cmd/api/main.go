f6c6f2a10122bb959b16d0c583c02673ef660e34
renesanchez455 <renesanchez455@yahoo.com>
2022-09-09 00:01:34 -0600

    Added flags, logger, app struct and servemux

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,12 @@
 // Filename: cmd/api/main.go
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
 
 // The application version number
 const version = "1.0.0"
@@ -19,5 +24,21 @@ type application struct {
 }
 
 func main() {
+	var cfg config
+	// read in the flags that are needed to populate our config
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development | staging | production)")
+	flag.Parse()
+	//Create a logger
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	//Create an instance of our application struct
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// Create our new servemux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
 
 }
