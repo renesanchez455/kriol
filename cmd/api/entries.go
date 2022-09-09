8802d2e195372841a9b7beec1ecd172100168f93
renesanchez455 <renesanchez455@yahoo.com>
2022-09-09 12:08:06 -0600

    added show EntryHandler and edited routes.go

--- cmd/api/entries.go
+++ cmd/api/entries.go
@@ -5,9 +5,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-// createEntryHandler for the "POST /v1/schools" endpoint
+// createEntryHandler for the "POST /v1/entries" endpoint
 func (app *application) createEntryHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "create a new entry..")
+}
+
+// showEntryHandler for the "GET /v1/entries/:id" endpoint
+func (app *application) showEntryHandler(w http.ResponseWriter, r *http.Request) {
+	// Use the "ParamsFromContext()" function to get a request context as a slice
+	params := httprouter.ParamsFromContext(r.Context())
+
+	// GET the value of the "id" parameter
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Display the entry id
+	fmt.Fprintf(w, "show the details for entry %d\n", id)
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,8 +12,8 @@ func (app *application) routes() *httprouter.Router {
 	// Create a new httprouter instance
 	router := httprouter.New()
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/entries", app.createEntriesHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/entries/:id", app.showEntriesHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/entries", app.createEntryHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/entries/:id", app.showEntryHandler)
 
 	return router
 }
