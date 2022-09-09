a4424743ea8a43db541ee18aafb43cda8bc1e05f
renesanchez455 <renesanchez455@yahoo.com>
2022-09-08 23:36:20 -0600

    Added application version, config settings to main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,23 @@
 // Filename: cmd/api/main.go
 package main
 
-import (
-	"fmt"
-)
+import "log"
+
+// The application version number
+const version = "1.0.0"
+
+// The configuration settings
+type config struct {
+	port int
+	env  string // development, staging, production, etc.
+}
+
+// Dependency injection
+type application struct {
+	config config
+	logger *log.Logger
+}
 
 func main() {
-	fmt.Println("A journey of a thousand miles begins with a single step.")
+
 }
